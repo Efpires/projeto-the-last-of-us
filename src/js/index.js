4cf685e0e66fbedb48de41d08708b1de2bdc0604
efpires <eduardo_fgp@hotmail.com>
2023-03-13 12:50:24 -0300

    final

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,33 @@
 const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
 
 botoesCarrossel.forEach((botao, indice) => {
     botao.addEventListener('click', () => {
-        const botaoSelecionado = document.querySelector('.selecionado')
-        botaoSelecionado.classList.remove('selecionado')
+        desativarBotaoSelecionado();
 
-        botao.classList.add('selecionado')
+        selecionarBotaoCarrossel(botao);
 
-        const imagemAtiva = document.querySelector('.ativa')
-        imagemAtiva.classList.remove('ativa')
+        esconderImagemAtiva();
 
-         
+        mostrarImagemDeFundo(indice);       
     })
 })
+
+function selecionarBotaoCarrossel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
 
